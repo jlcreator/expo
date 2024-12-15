67b45d37456723cf9bfefc1c6114b57a8dfa68e8
James Lee <70414905+jlcreator@users.noreply.github.com>
2024-12-15 11:40:10 +1100

    Create expo-router.tsx

---
+++ expo-router.tsx
@@ -0,0 +1,13 @@
+// Slot, Stack, Tabs
+
+import { Tabs, Stack, Slot } from 'expo-router';
+
+export default function RootLayout() {
+  <Tabs>
+    <Tabs.Screen name=""
+      // title
+      // headerShown <Boolean>
+      // tabBarIcon
+      options={{ title: "", tabBarIcon: ()  }}></Tabs.Screen>
+  </Tabs>
+}
